ae877db61918b744b1ab8dfc5782a4ad04ff91fa
Heshan19 <86555837+Heshan19@users.noreply.github.com>
2022-02-02 23:39:14 +0530

    Update package-details.php

--- package-details.php
+++ package-details.php
@@ -64,7 +64,7 @@ $error="Something went wrong. Please try again";
 		.errorWrap {
     padding: 10px;
     margin: 0 0 20px 0;
-    background: #fff;
+    background: white;
     border-left: 4px solid #dd3d36;
     -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
     box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
@@ -84,7 +84,7 @@ $error="Something went wrong. Please try again";
 <?php include('includes/header.php');?>
 <div class="banner-3">
 	<div class="container">
-		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> Package Details</h1>
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">Package Details</h1>
 	</div>
 </div>
 <!--- /banner ---->
